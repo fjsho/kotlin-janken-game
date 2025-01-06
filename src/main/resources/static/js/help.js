3cff237a3e10a03b5df944660e4fa1942d448fa6
fjsho <81564274+fjsho@users.noreply.github.com>
2025-01-06 17:17:08 +0900

    add: ヘルプページ作成

---
+++ src/main/resources/static/js/help.js
@@ -0,0 +1,6 @@
+/* help.js */
+// 「ホームに戻る」ボタンのイベント
+const homeButton = document.getElementById('homeButton');
+homeButton.addEventListener('click', () => {
+window.location.href = 'index.html'; // ホーム画面に戻る
+});
